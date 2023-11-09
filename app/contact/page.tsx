bb04951452bfb790b0b77ad42d428d918be6809c
mamun-mahmood <mamamun1999@gmail.com>
2023-11-09 09:08:32 +0600

    added work and contact route

---
+++ app/contact/page.tsx
@@ -0,0 +1,15 @@
+import {FC} from 'react';
+
+interface pageProps {
+  // Define your component props here
+}
+
+const page: FC<pageProps> = (props) => {
+  return (
+    <div>
+      contact page
+    </div>
+  );
+};
+
+export default page;    
